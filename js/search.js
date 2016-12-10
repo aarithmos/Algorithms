2f85be8fc24efa29c8bdc60e47b0a65de6dc8b0e
aarithmos <grohan952@gmail.com>
2016-12-10 19:50:37 +0530

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,7 +1,6 @@
 jQuery(function() {
   // Initialize lunr with the fields to be searched, plus the boost.
   window.idx = lunr(function () {
-    this.field('id');
     this.field('title');
     this.field('content', { boost: 10 });
     this.field('author');
@@ -43,7 +42,7 @@ jQuery(function() {
           var item = loaded_data[result.ref];
 
           // Build a snippet of HTML for this result
-          var appendString = '<li><a href="' + item.url + '">' + item.title + '</a></li>';
+          var appendString = '<h3 class="ab">You might find what you are looking for in this category</h3><li><a href="' + item.url + '">' + item.title + '</a></li>';
 
           // Add the snippet to the collection of results.
           $search_results.append(appendString);
